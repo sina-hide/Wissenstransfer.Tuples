07865929aa8447e96a1a048ef88dfa8000a4ccdd
Sina Hinderks <sina@hinderks.net>
2019-09-26 22:14:41 +0200

    swap example

---
+++ Swap.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Tuples
+{
+    [Run(2)]
+    public class Swap
+    {
+        public Swap()
+        {
+            var data = new Data("Skeet","Jon", "C# in Depth");
+            (data.FirstName, data.LastName) = (data.LastName, data.FirstName);
+            Console.WriteLine($"{data.FirstName} {data.LastName}: {data.Title}");
+        }
+
+        private struct Data
+        {
+            public Data(
+                string firstName,
+                string lastName,
+                string title)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Title = title;
+            }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Title { get; set; }
+        }
+    }
+}
